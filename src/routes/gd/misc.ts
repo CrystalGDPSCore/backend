4a11da8200e22c57c6042fa88e156da82913efb8
inREZy <41942304+inREZy@users.noreply.github.com>
2024-03-05 17:53:47 +0800

    Delete src/routes/gd/misc.ts

--- src/routes/gd/misc.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from "fastify";
-
-import { requestUserAccessHandler } from "../../controllers/gd/misc";
-
-export default async function gdMiscRoutes(fastify: FastifyInstance) {
-    fastify.post("/requestUserAccess.php", requestUserAccessHandler);
-}
