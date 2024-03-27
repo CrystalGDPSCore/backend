7d848d284bffeef7d91d9624e6bf7016e85fd955
inrezy <xardmax2000@gmail.com>
2024-03-27 11:02:29 +0800

    added getRelativeTime function

---
+++ src/utils/relativeTime.ts
@@ -0,0 +1,26 @@
+export function getRelativeTime(date: Date) {
+    const timeDifference = Math.floor((Date.now() - date.getTime()) / 1000);
+
+    const units = {
+        year: 24 * 60 * 60 * 365,
+        month: 24 * 60 * 60 * 365 / 12,
+        day: 24 * 60 * 60,
+        hour: 60 * 60,
+        minute: 60,
+        second: 1,
+    };
+
+    const rtf = new Intl.RelativeTimeFormat("en", { numeric: "always" });
+
+    for (const [unit, seconds] of Object.entries(units)) {
+        if (timeDifference >= seconds || unit == "second") {
+            const time = rtf.format(-(Math.floor(timeDifference / seconds)), unit as Intl.RelativeTimeFormatUnit).slice(0, -4);
+
+            if (time == "0 seconds") {
+                return "1 second";
+            }
+
+            return time;
+        }
+    }
+}
